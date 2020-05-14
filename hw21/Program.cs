82864938cf7651f122589fc128311ebac357cab0
User <payrav-343@mail.ru>
2020-05-14 09:03:46 +0500

    Добавьте файлы проекта.

---
+++ hw21/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Collections.Immutable;
+
+using System.ComponentModel.Design;
+
+using System.Linq;
+
+using System.Runtime.CompilerServices;
+
+namespace Hw21
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            //Задача 1
+
+            
+            //    Console.WriteLine("Наберите слово => aeiou");
+            //    Console.WriteLine(new string('=',25));
+            //    string letter = "aeiou";
+
+            //    string input = Console.ReadLine();
+
+            //    var res = new string(input.Select((i) => (letter.Contains(i) ? 
+            //                        ((char)((int)48 + letter.IndexOf(i) + 1)) 
+            //                        : i)).ToArray());
+
+            //    Console.WriteLine(res);
+    
+            
+        }
+
+    }
+
+}
